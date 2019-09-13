c1aadc2fceca7d5d890d965d46c9ff2eb83d930d
Jalysa Reason <jalysareason@gmail.com>
2019-09-13 17:40:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
   end
 
 def add_element_to_end_of_array(array, element)
-  
+   music_genre =["R&B","Hip Hop","Pop","Techno"]
 end
 
 def add_element_to_start_of_array(array, element)
