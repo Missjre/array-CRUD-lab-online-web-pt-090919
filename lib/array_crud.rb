a12878eeec56f229ccb4dfdcd0cb88382c5a04fb
Jalysa Reason <jalysareason@gmail.com>
2019-09-13 17:41:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  music_genre =["R&B","Hip Hop","Pop","Techno"]
+   music_genre.push("Gospel")
 end
 
 def remove_element_from_end_of_array(array)
